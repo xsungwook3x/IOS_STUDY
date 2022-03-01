ca11b78417ee26ac50935dfb0208a6776eea520e
Seonguk <xsungwook3x@naver.com>
2022-03-01 17:52:54 +0900

    Lottie animation을 이용한 인트로페이지 만들어보기

---
+++ NiceApp/NiceApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiceApp
+//
+//  Created by 김성욱 on 2022/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
